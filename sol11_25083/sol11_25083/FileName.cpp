032d7f80fbbf2f5b1e235cb19ef9ea1d4790e6ee
junyoung365 <135by135by@gmail.com>
2026-03-03 17:03:28 +0900

    commit sol11
    .

---
+++ sol11_25083/sol11_25083/FileName.cpp
@@ -0,0 +1,40 @@
+/*
+* 문제
+아래 예제와 같이 새싹을 출력하시오.
+
+입력
+입력은 없다.
+
+출력
+새싹을 출력한다.
+
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+* 
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    cout << "         ,r'\"7";// : ", \ 등의 파싱: \을 앞에 붙이기
+    cout << endl;
+    cout << "r`-_   ,'  ,/";
+    cout << endl;
+    cout << " \\. \". L_r'";
+    cout << endl;
+    cout << "   `~\\/";
+    cout << endl;
+    cout << "      |";
+    cout << endl;
+    cout << "      |";
+
+}
